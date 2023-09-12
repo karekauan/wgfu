ad575f74ebaac256464566f4e511dda05bbb2be5
karekauan <costakauanantonye@gmail.com>
2023-09-12 20:17:06 -0300

    Starting Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'wgfu';
+export class AppComponent implements OnInit {
+  formatted_hour = '';
+
+  ngOnInit() {
+    setInterval(() => {
+      this.updateTime();
+    }, 1000);
+  }
+
+  updateTime() {
+    const date = new Date();
+    date.setHours(date.getHours());
+    const hours = this.padZero(date.getHours());
+    const minutes = this.padZero(date.getMinutes());
+    const seconds = this.padZero(date.getSeconds());
+
+    this.formatted_hour = `${hours}:${minutes}:${seconds}`;
+  }
+
+  padZero(num: number) {
+    return num < 10 ? `0${num}` : `${num}`;
+  }
 }
